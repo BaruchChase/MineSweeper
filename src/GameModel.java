e6361c2ccf64aeb84e691fe33495fcaa56990813
shaulniyazov <sniyazov2@student.touro.edu>
2023-03-02 12:13:46 -0500

    finished the setUpMines method to work with the 1 dimensional array

--- src/GameModel.java
+++ src/GameModel.java
@@ -7,22 +7,21 @@ public class GameModel {
 
     public void setUpMines(){
         //loop through the array with a random number connoting chance of mine
-        //if higher than 25%, mine, continue, if less, mine
+        //25% chance of a cell being a mine
         int amountOfMines = gameBoard.getMineNumber();
         Random rand = new Random();
-        for (int i = 0; i < gameBoard.boardSize; i++) {
-            int mine = rand.nextInt(4);
-            if(mine == 1){
-                //todo Cell= Mine
+        //make sure all the mines are placed by restarting the loop if there are still mines
+        while(amountOfMines != 0) {
+            for (int i = 0; i < gameBoard.boardSize; i++) {
+                int mine = rand.nextInt(4);
+                //if this is a second round of placement, make sure it's not being placed in the same spot.
+                if (mine == 1 && gameBoard.board[i].getCellVal() != 9) {
+                    //A 9 indicates a bomb because that is a number that can't appear
+                    gameBoard.board[i].setCellVal(9);
+                    amountOfMines--;
+                }
             }
         }
 
     }
 }
-/*
-while(amountOfMines != 0){
-            int place = rand.nextInt(gameBoard.boardSize);
-            //if place is empty, then make it a bomb
-            //then mines--;
-        }
- */
